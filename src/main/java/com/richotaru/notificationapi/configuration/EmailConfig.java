7ecde7781e8fba7bde9b649d643b60c5d61a3e9b
rotaru <rotaru@byteworks.com.ng>
2021-05-03 12:35:40 +0100

    added-email testing credentials

--- src/main/java/com/richotaru/notificationapi/configuration/EmailConfig.java
+++ src/main/java/com/richotaru/notificationapi/configuration/EmailConfig.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Data
 public class EmailConfig {
-    @Value("${EMAIL_NOTIFICATION_ADDRESS:info@irembo.com}")
+    @Value("${EMAIL_NOTIFICATION_ADDRESS:postmaster@sandbox929d588bfe4c42fb9fc54a3b55b9e3fe.mailgun.org}")
     public String EMAIL_NOTIFICATION_ADDRESS;
-    @Value("${EMAIL_NOTIFICATION_PASSWORD:API-KEY-de3f47db99fe65cf51ea8747effc3e5c}")
+    @Value("${EMAIL_NOTIFICATION_PASSWORD:91e2940fe97ee4e0c2832409f9a0a788-2a9a428a-742ee7e5}")
     public String EMAIL_NOTIFICATION_PASSWORD;
     @Value("${SMTP_PORT:587}")
     public Integer SMTP_PORT;
-    @Value("${SMTP_HOST:API-KEY-de3f47db99fe65cf51ea8747effc3e5c}")
+    @Value("${SMTP_HOST:smtp.mailgun.org}")
     public String SMTP_HOST;
 
 }
